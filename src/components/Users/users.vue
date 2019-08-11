3a026b085bae47143213e7c1d42a400cc5800e68
toby1123yjh <525614953@qq.com>
2019-08-12 00:06:21 +0800

    update

--- src/components/Users/users.vue
+++ src/components/Users/users.vue
@@ -9,11 +9,13 @@
       </el-breadcrumb>
       <!--搜索-->
       <el-row class="search-row">
-        <el-col >
-            <el-input placeholder="请输入内容" v-model="query" class="input-with-select">
-              <el-button slot="append" icon="el-icon-search"></el-button>
-            </el-input>
-            <el-button type=" primary">添加用户</el-button>
+        <el-col>
+          <el-input
+            @clear="loadUserList()"
+            clearable placeholder="请输入内容" @click="searchUser()" v-model="query" class="input-with-select">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+          <el-button type=" primary" @click="addUserDia()">添加用户</el-button>
         </el-col>
       </el-row>
 
@@ -42,6 +44,38 @@
         </el-table-column>
       </el-table>
       <!--分页-->
+      <el-pagination class="block"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pagenum"
+        :page-sizes="[2, 4, 6, 8]"
+        :page-size="2"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+
+      <!--添加用户对话框-->
+      <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd">
+        <el-form :model="form">
+          <el-form-item label="用户名" label-width="100px">
+            <el-input v-model="form.username" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" label-width="100px">
+            <el-input v-model="form.password" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="电话" :label-width="formLabelWidth">
+            <el-input v-model="form.mobile" autocomplete="off"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+          <el-button type="primary" @click="dialogFormVisibleAdd = false">确 定</el-button>
+        </div>
+      </el-dialog>
+
     </el-card>
   </div>
 </template>
@@ -50,8 +84,28 @@
     export default {
         data() {
             return {
-                query:'',
+                //查询数据
+                query: '',
+                //分页数据
+                pagenum: 1,  //当前页
+                pagesize: 2,
+                total: 6,
+                //添加对话框的属性
+                formLabelWidth:'100px',
+                dialogFormVisibleAdd:false,
+                //添加用户表单数据
+                form:{
+                    username:"",
+                    password:"",
+                    email:"",
+                    mobile:"",
+                },
+                //列表数据
                 tableData: [{
+                    date: '2016-05-02',
+                    name: 'toby',
+                    address: '荔湾区站前路22'
+                }, {
                     date: '2016-05-02',
                     name: 'toby',
                     address: '荔湾区站前路22'
@@ -62,10 +116,52 @@
         created() {
             this.getUserList()
         },
-        methods :{
+        methods: {
+
             async getUserList() {
-            //    this.&http
-                const res =await this.$http.get('users?query=')
+                const AUTH_TOKEN = localStorage.getItem('token')
+                this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
+                //const res =await this.$http.get('users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}')
+            },
+            //分页方法
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
+            },
+            //搜索用户，query是双向绑定的，在getUserList()方法中已经写了
+            searchUser() {
+                this.getUserList()
+            },
+            //重新加载列表数据
+            loadUserList() {
+                this.getUserList()
+            },
+            //显示添加对象对话框
+            addUserDia(){
+                this.dialogFormVisibleAdd=true
+            },
+            //删除用户
+            showDeleUserMsgBox(userId){
+                this.$confirm('删除用户？','提示',{
+                    confiremButton:'确定',
+                    cancelButtonText:'确定',
+                    type:'warning'
+                }).then(async ()=>{
+                    const res=await this.$http.delete('users/${userId}')
+                    console.log(reg)
+                    if(res.data.meta.status===200){
+                        this.getUserList()
+                        this.$message({
+                            type:'success',
+                            message:res.data.meta.msg
+
+                        })
+                    }
+                }).catch(()=>{
+                    this.$http.
+                })
             }
         }
     }
@@ -75,10 +171,17 @@
   .box-card {
     height: 100%;
   }
-  .input-with-select{
+
+  .input-with-select {
     width: 400px;
   }
-  .search-row{
-    margin-top:40px;
+
+  .search-row {
+    margin-top: 40px;
+  }
+
+  .block {
+    text-align: center;
+    margin-top: 50px;
   }
 </style>
